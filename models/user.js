8c56e2462a1c731e0d2d86b6c3653597892ab840
squaredbusinessman <burdaonelove@gmail.com>
2022-09-20 13:00:48 +0300

    feat: Создаёт схему-модель пользователя, подключается к базе данных MongoDB

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: 'string',
+    required: [true, 'Поле имя должно быть заполнено!'],
+    minLength: 2,
+    maxLength: 30,
+  },
+  email: {
+    type: 'string',
+    required: [true, 'Поле email должно быть заполнено!'],
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Пожалуйста, введите корректный email',
+    },
+  },
+  password: {
+    type: 'string',
+    required: [true, 'Поле пароль должно быть заполнено!'],
+    select: false,
+  },
+})
+
+module.exports = mongoose.model('user', userSchema);
