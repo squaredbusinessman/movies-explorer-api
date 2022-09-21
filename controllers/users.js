d7f4d16ec70cb919fb86e0d42401e31533ecbbbc
squaredbusinessman <burdaonelove@gmail.com>
2022-09-21 11:23:19 +0300

    feat: Создаёт контроллеры user, подключает их к роутам

--- controllers/users.js
+++ controllers/users.js
@@ -2,3 +2,58 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const errorsCodes = require('../errors/errorCodes');
+const ExistingDataError = require("../errors/ExistingDataError");
+const ValidationError = require("../errors/ValidationError");
+
+const createUser = (req, res, next) => {
+  const {
+    name, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({ name, email, password: hash, }))
+    .then((user) => {
+      const userWithoutPass = user.toObject();
+      delete userWithoutPass.password;
+      res.status(201).send(userWithoutPass);
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new ValidationError('Введены некорректные данные при создании пользователя'))
+      } else if (error.code === errorsCodes.DuplicateErrorCode) {
+        next(new ExistingDataError('пользователь с данным email уже зарегистрирован'));
+      } else {
+        next(error);
+      }
+    })
+    .catch(next);
+};
+
+const getUserData = (req, res, next) => {
+  const id = req.user._id;
+  User.findById(id)
+    .then(user => res.send(user))
+    .catch(next);
+};
+
+const patchUserData = (req, res, next) => {
+  User.findByIdAndUpdate(req.user._id, {
+    name: req.body.name,
+  }, {
+    runValidators: true,
+    new: true,
+  })
+    .then(user => res.send(user))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new ValidationError('Введены некорректные данные для обновления информации о пользователе'))
+      } else {
+        next(error);
+      }
+    });
+}
+
+module.exports = {
+  createUser,
+  getUserData,
+  patchUserData,
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { userPatchValidate } = require('../middlewares/validators');
+const { getUserData, patchUserData } = require('../controllers/users');
 
-router.get('/users/me', );
+router.get('/users/me', getUserData);
 
-router.patch('/users/me', userPatchValidate, );
+router.patch('/users/me', userPatchValidate, patchUserData);
